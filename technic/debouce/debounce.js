c40ffce8cd471f6373eacd95eb326b30675ebaff
hyunwoo.jo <hyunwoo.jo@navercorp.com>
2018-10-17 00:39:55 +0900

    debounce

---
+++ technic/debouce/debounce.js
@@ -0,0 +1,12 @@
+
+const debounce = (fn, ms = 0) => {
+
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  }
+}
+
+
